abb99f71a5d3789b63c001a2a182c1e2de6a91fa
PleaseAttainBuddhaHood <oksk9633@gmail.com>
2022-12-21 04:18:11 +0900

    ~ p181

---
+++ src/main/java/com/Spring_Board/domain/SampleDTO.java
@@ -0,0 +1,10 @@
+package com.Spring_Board.domain;
+
+import lombok.Data;
+
+@Data
+public class SampleDTO 
+{
+  private String name;
+  private int age;
+}
---
+++ src/main/java/com/Spring_Board/domain/SampleDTOList.java
@@ -0,0 +1,19 @@
+package com.Spring_Board.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class SampleDTOList 
+{
+  private List<SampleDTO> list;
+  
+  
+  public SampleDTOList() 
+  {
+    list = new ArrayList<>();
+  }
+  
+}
